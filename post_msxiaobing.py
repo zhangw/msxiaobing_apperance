3b76795f1d4de0fc41f450ca25cf5f87b30a6be8
zhangw <jimokanghanchao@gmail.com>
2015-12-17 11:43:46 +0800

    增加了使用图片URL进行颜值计算的实现

--- post_msxiaobing.py
+++ post_msxiaobing.py
@@ -11,23 +11,33 @@ import urllib
 import urllib2
 import base64
 import json
+import logging
+logging.basicConfig(level=logging.INFO)
 
-def main():
-  path = "/Users/vincent/Others/pics_face/testpic/IMG_4032.PNG"
-  print "使用测试图片%s" % path
-  calculate_apperance(path)
-
-def rating_apperance(path):
-  print "正在上传图片计算颜值..."
-  image_uploaded = upload_image(path)
-  rst = analyze_image(image_uploaded,"yanzhi")['content']['text']
+def rating_apperance(path, local=True):
+  rst = ''
+  if local:
+    print "正在上传图片计算颜值..."
+    image_uploaded = upload_image(path)
+    rst = analyze_image(image_uploaded,"yanzhi")
+  else:
+    rst = analyze_image(url=path)
   print rst
-  import re
-  mark = re.findall(r"\d+\.?\d?",rst)
-  if len(mark) >= 1:
-    mark = float(mark[0]) * 10
-    print "机器人小冰计算的颜值:%s" % mark
-
+  if rst is not None:
+    rst = rst['content']['text']
+    import re
+    mark = re.findall(r"\d+\.?\d?",rst)
+    if len(mark) >= 1:
+      mark = float(mark[0]) * 10
+      logging.info("机器人小冰计算的颜值:%s" % mark)
+      return mark
+    else:
+      logging.warn("颜值计算没有返回得分")
+      return None
+  else:
+    logging.error("上传图片失败,或者请求图片URL失败")
+    return None
+  
 
 def upload_image(path):
   url = "http://kan.msxiaobing.com/Api/Image/UploadBase64"
@@ -40,12 +50,18 @@ def upload_image(path):
     if res.getcode() == 200:
       return json.loads(res.readlines()[0])
 
-def analyze_image(imageinfo,type="yanzhi"):
+def analyze_image(imageinfo=None,type="yanzhi",url=None):
   """
   type = ("howold","yanzhi"),howold:分析年龄，yanzhi:计算颜值。
+  imageinfo, 本地图片上传之后的图片数据
+  url, 远程图片的请求地址
   """
+  image_url = None
   if imageinfo is not None:
     image_url = imageinfo['Host'] + imageinfo['Url']
+  else:
+    image_url = url
+  if image_url is not None:
     url = "http://kan.msxiaobing.com/Api/ImageAnalyze/Process?service=%s&tid=%s" % (type,uuid.uuid4().hex)
     tv = time.time()
     tsmp = int(tv)
@@ -65,4 +81,4 @@ def analyze_image(imageinfo,type="yanzhi"):
       return json.loads(res.readlines()[0])
 
 if __name__ == '__main__':
-  main()
+  rating_apperance('http://ww1.sinaimg.cn/mw1024/005IrpZYgw1ew3696f218j30qo0zkjwn.jpg', local=False)
