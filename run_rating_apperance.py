0f918f568566e5f9d647e28d141397d75cc2469a
zhangw <jimokanghanchao@gmail.com>
2015-12-17 11:44:47 +0800

    增加了命令行参数--url用于图片的URL地址，和--path为一组互斥的必填参数

--- run_rating_apperance.py
+++ run_rating_apperance.py
@@ -11,11 +11,16 @@ import argparse
 
 def main():
   parser = \
-  argparse.ArgumentParser(description="根据输入的本机图片路径，借用微软小冰计算人[http://kan.msxiaobing.com/ImageGame/Portal?task=howold]测算颜值.")
-  parser.add_argument("--path", type=str, help="输入本机的图片全路径，比如--path /Users/xxx/xxx.jpg")
+  argparse.ArgumentParser(description="根据输入的本机图片路径/图片的URL，借用微软小冰计算人[http://kan.msxiaobing.com/ImageGame/Portal?task=yanzhi]测算颜值.")
+  #--path and --url should be in a mutually exclusive group
+  group = parser.add_mutually_exclusive_group(required=True)
+  group.add_argument("--path", type=str, help="输入本机的图片全路径，比如--path /Users/xxx/xxx.jpg")
+  group.add_argument("--url", type=str, help="输入图片的URL，比如--url http://ww2.sinaimg.cn/mw1024/7d01636bjw1ew43tsta3kj20ak0bjaag.jpg")
   args = parser.parse_args()
   if args.path is not None:
     rating_apperance(args.path)
+  elif args.url is not None:
+    rating_apperance(args.url, local=False)
   else:
     parser.print_help()
 
